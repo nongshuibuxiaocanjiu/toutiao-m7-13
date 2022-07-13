b5b8d3789933f060f4408d3742971a15047271ac
jin <2293748571@qq.com>
2022-07-13 12:07:09 +0800

    完成初始化

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    'postcss-pxtorem': {
+          rootValue: (module) => /vant/gi.test(module.file) ? 37.5 : 75,
+      propList: ['*']
+    }
+  }
+}
+
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/fonts/iconfont.css'
+import 'amfe-flexible/index.min.js'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
